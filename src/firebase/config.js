8ac9dd9c9f72858c7d61bb2b081ba4586b77ec09
bgleoquip <bgleoquip@gmail.com>
2020-12-28 19:53:19 +0530

    process.env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,10 @@
 // firebase config
 export const firebaseConfig = {
-    apiKey: "AIzaSyCdw-Ko_nIcjW7_DjvdK2C1U-kxG0bODPg",
-    authDomain: "fir-login-82579.firebaseapp.com",
-    databaseURL: "https://fir-login-82579.firebaseio.com",
-    projectId: "fir-login-82579",
-    storageBucket: "fir-login-82579.appspot.com",
-    messagingSenderId: "939975206218",
-    appId: "1:939975206218:web:22f01ebab34bb0c0ca4db3"
+    apiKey: process.env.apiKey || "AIzaSyCdw-Ko_nIcjW7_DjvdK2C1U-kxG0bODPg",
+    authDomain: process.env.authDomain || "fir-login-82579.firebaseapp.com",
+    databaseURL: process.env.databaseURL || "https://fir-login-82579.firebaseio.com",
+    projectId: process.env.projectId || "fir-login-82579",
+    storageBucket: process.env.storageBucket || "fir-login-82579.appspot.com",
+    messagingSenderId: process.env.messagingSenderId || "939975206218",
+    appId: process.env.appId || "1:939975206218:web:22f01ebab34bb0c0ca4db3"
   };
